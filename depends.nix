a2cbc8a301e73bcdfe121fd7c054b9b13066e8db
0xb10c <b10c@b10c.me>
2023-10-17 13:15:09 +0200

    change: switch to bitcoincore.org depends mirror

--- depends.nix
+++ depends.nix
@@ -132,7 +132,8 @@ let
       sha256 = "5af07de982ba658fd91a03170c945f99c971f6955bc79df3266544373e39869c";
     };
     zeromq = {
-      urlPrefix = "https://github.com/zeromq/libzmq/releases/download/v4.3.4";
+      #urlPrefix = "https://github.com/zeromq/libzmq/releases/download/v4.3.4";
+      urlPrefix = "https://bitcoincore.org/depends-sources/";
       file = "zeromq-4.3.4.tar.gz";
       sha256 = "c593001a89f5a85dd2ddf564805deb860e02471171b3f204944857336295c3e5";
     };
@@ -142,8 +143,8 @@ let
       sha256 = "12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef";
     };
     miniupnpc = {
-      urlPrefix = "https://miniupnp.tuxfamily.org/files/";
-      file = "miniupnpc-2.2.2.tar.gz";
+      urlPrefix = "https://bitcoincore.org/depends-sources/";
+      file="miniupnpc-2.2.2.tar.gz";
       sha256 = "888fb0976ba61518276fe1eda988589c700a3f2a69d71089260d75562afd3687";
     };
     libnatpmp = {
