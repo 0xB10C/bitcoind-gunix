4f0a62df33301e4a25d220f5fd76de1020b30db3
0xb10c <github@b10c.me>
2024-07-10 16:26:52 +0200

    update: bitcoin coreto v27.0

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  version = "26.0";
+  version = "27.0";
   url = "https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}.tar.gz";
-  sha256 = "sha256-qx2ZJ24o22LR2fOQHoWsNY1/Hry5QtNIqcTkbw/NwKE=";
+  sha256 = "sha256-nB7mUdOxV7rMwziL4ouM87/O/NJJO5Q3Ja1gQMprFGs=";
 
   depends = pkgs.callPackage ./depends.nix { inherit version url sha256; };
   bitcoind = pkgs.callPackage ./bitcoind.nix { inherit url sha256 depends; };
